45afcbed4b6a03b7f0f39a074b3ab1d5882a8d3c
dunglucbac <luu.quang-thang@outlook.com>
2021-12-04 12:31:28 +0700

    added recipe detail

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -29,3 +29,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredient', 'tags', 'time_minutes', 'price',
                   'link')
         read_only_fields = ('id', )
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
